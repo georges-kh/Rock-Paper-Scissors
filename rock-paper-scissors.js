2c6a0565438b8051377278a65c1fdc830f4c1eaf
Georges Khoueiry <georges.nkhoueiry@gmail.com>
2022-12-19 18:56:49 +0200

    Create playerRound function
    plays one round of rps with the computer and returns a message depending on the result
    
    Create game function
    
    plays 5 rounds while keeping track of score

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,33 @@
 function getComputerChoice() {
-    let moves = ["Rock", "Paper", "Scissors"];
-    compMove = moves[Math.floor(Math.random() * moves.length)];
-    return compMove;
+	const moves = ["Rock", "Paper", "Scissors"];
+	compChoice = moves[Math.floor(Math.random()*moves.length)];
+	return compChoice
 }
 
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice().toUpperCase();
+
+
+function playerRound(playerSelection, computerSelection) {
+	let winningMoves = {"ROCK" : "SCISSORS", "SCISSORS" : "PAPER", "PAPER" : "ROCK"};
+	let playerScore = 0;
+	let computerScore = 0;
+	if (playerSelection === computerSelection) {
+		playerScore += 1;
+		computerScore += 1;
+		return "It's a Draw!";
+	} else if (winningMoves[playerSelection] === computerSelection) {
+		playerScore++
+		return `You win! ${playerSelection} beats ${computerSelection}`;
+	} else {
+		computerScore++
+		return `You lose! ${computerSelection} beats ${playerSelection}`;
+	}
+}
+
+function game(rounds) {	
+	for (let i = 0; i < rounds; i++) {
+		let playerSelection = prompt("What's your move?", "").toUpperCase();
+		playerRound(playerSelection, computerSelection);
+		console.log(`The current score is:\nPlayer: ${playerScore} points\nComputer: ${computerScore}`)
+	}
+}
